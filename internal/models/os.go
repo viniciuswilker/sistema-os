dcba8289edd2c298adf25f9f46058be131e79c6c
vinicius wilker <vinicius.zoom360@gmail.com>
2025-12-16 21:22:05 -0300

    add models de os

---
+++ internal/models/os.go
@@ -0,0 +1,17 @@
+package models
+
+import "gorm.io/gorm"
+
+type OrdemServico struct {
+	gorm.Model
+
+	ClientID uint `gorm:"not null" json:"cliente_id"`
+
+	Cliente Cliente `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"cliente,omitempty"`
+
+	Aparelho    string  `gorm:"not null" json:"aparelho"`
+	Defeito     string  `gorm:"not null" json:"defeito"`
+	Status      string  `gorm:"default:'Recebido'" json:"status"`
+	Valor       float64 `json:"valor"`
+	Observacoes string  `json:"observacoes"`
+}
