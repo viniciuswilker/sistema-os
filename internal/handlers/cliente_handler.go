e1d17f54eabd817efb1a266568632756b3b08386
vinicius wilker <vinicius.zoom360@gmail.com>
2025-12-16 21:13:58 -0300

    cliente handler att

--- internal/handlers/cliente_handler.go
+++ internal/handlers/cliente_handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"sistema-os/internal/models"
 	"sistema-os/internal/repository"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -30,5 +31,25 @@ func (h *ClienteHandler) CriarCliente(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, cliente)
+
+}
+
+func (h *ClienteHandler) BuscarCliente(c *gin.Context) {
+	idParam := c.Param("id")
+
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"erro": "ID inválido"})
+		return
+	}
+
+	cliente, err := h.repo.BuscarPorID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"erro": "Cliente não encontrado"})
+		return
+	}
+
+	c.JSON(http.StatusOK, cliente)
 
 }
